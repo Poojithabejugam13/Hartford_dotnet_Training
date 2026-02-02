492ddb773faec70786ffa9269c2c267f5df2c9bb
Poojitha <162865478+Poojithabejugam13@users.noreply.github.com>
2026-02-02 22:54:33 +0530

    Add files via upload

---
+++ DAY23-Assignment/typeinfr.cs
@@ -0,0 +1,16 @@
+﻿//typeinference
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        var number = 100;
+        var name = "CSharp";
+        var price = 99.99;
+
+        Console.WriteLine(number.GetType());
+        Console.WriteLine(name.GetType());
+        Console.WriteLine(price.GetType());
+    }
+}
