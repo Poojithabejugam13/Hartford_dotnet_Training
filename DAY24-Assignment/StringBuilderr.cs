00e2151d9a8ef3fd59afc2a26a3475be9a1d2023
Poojitha <162865478+Poojithabejugam13@users.noreply.github.com>
2026-02-03 11:03:23 +0530

    Add files via upload

---
+++ DAY24-Assignment/StringBuilderr.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Text;
+
+namespace stringbuilder
+{
+    class StringBuilderr
+    {
+        static void Main()
+        {
+            string s1 = "Hello";
+            string s2 = "World";
+
+            string result = s1 + " " + s2;
+            Console.WriteLine(result);
+            Console.WriteLine(result.ToUpper());
+            Console.WriteLine("Length = " + result.Length);
+
+            StringBuilder sb = new StringBuilder();
+            sb.Append("Hello");
+            sb.Append(" ");
+            sb.Append("World");
+
+            Console.WriteLine(sb.ToString());
+        }
+    }
+
+}
---
+++ DAY24-Assignment/formatting.cs
@@ -0,0 +1,28 @@
+﻿//formatting
+using System;
+
+namespace formatting
+{
+    class Program
+    {
+        static void Main()
+        {
+            int marks = 456;
+            int total = 500;
+            double percentage = (marks * 100.0) / total;
+            Console.WriteLine("Marks : {0}/{1}", marks, total);
+            Console.WriteLine("Percentage : {0:F2}%", percentage);
+            string name = "Ravi";
+            int age = 22;
+            double salary = 45000.5;
+
+            string result = String.Format(
+                "Name: {0}, Age: {1}, Salary: {2:F2}",
+                name, age, salary
+            );
+
+            Console.WriteLine(result);
+        }
+    }
+
+}
